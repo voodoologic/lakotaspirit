ae11c029c3da05280ad257034cb0c5cf89a994b6
Douglas Headley <headley.douglas@gmail.com>
2010-12-30 12:21:27 -0700

    working now?

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ActiveRecord::Base
-	acts_as_authentic
-	has_many :items
-	def full_name
-		[first_name, last_name].join(" ")	
-	end
-	has_attached_file :photo, :styles => { :small => "150x150>" , :medium => "300x300>", :large => "640x480>", :thumb => "50x50#"},
-                  :url  => "/assets/bio/:id/:style/:basename.:extension"
-end
--- test/blueprints/user.rb
+++
@@ -1,6 +0,0 @@
-User.blueprint do
-  login { Faker::Internet.user_name }
-  password 'testing'
-  password_confirmation { password }
-  email { Faker::Internet.email }
-end
